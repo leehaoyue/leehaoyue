ce6a712bf5df198f8f6f23f24d3990f5188f6489
”leehaoyue“ <1946188354@qq.com>
2019-08-22 20:59:24 +0800

    修改相对路径，解决打包空白页问题；添加全局数据等

--- EMP/react-antd/service-worker.js
+++ EMP/react-antd/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4a14536d4109bf5b501dc9313736265d.js"
+  "./precache-manifest.fe0b7240d4757118e09d00b9143a6999.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
