8a383286da409e12b521dc08d3e30f7c9eb30846
皇阿玛 <1946188354@qq.com>
2020-03-08 19:34:50 +0800

    修改gzip配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = {
   publicPath: './',
   outputDir: 'dist',
@@ -56,5 +58,16 @@ module.exports = {
   chainWebpack: config => {
     config.plugins.delete('preload')
     config.plugins.delete('prefetch')
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [new CompressionPlugin({
+          test: /\.js$|\.html$|\.css/,
+          threshold: 10240,
+          deleteOriginalAssets: false
+        })]
+      }
+    }
   }
 }
